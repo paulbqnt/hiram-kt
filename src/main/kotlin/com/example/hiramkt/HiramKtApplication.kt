7a45cf1a18f2c71ce7abc4ee01d1a5ea443012c1
Paul Boquant <paul.boquant@chartered-investment.com>
2024-10-27 15:49:26 +0100

    feat(controller): add initial MessageController

---
+++ src/main/kotlin/com/example/hiramkt/HiramKtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hiramkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HiramKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<HiramKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hiramkt/HiramKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hiramkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HiramKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
